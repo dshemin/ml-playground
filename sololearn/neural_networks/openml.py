b98c21900098001d28993d0ccccae3e65a6d5edf
Dmitry Shemin <shemindmitry@gmail.com>
2023-02-03 20:22:55 +0700

    finish sololearn course

--- sololearn/neural_networks/openml.py
+++ sololearn/neural_networks/openml.py
@@ -3,3 +3,10 @@ X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 
 X5 = X[y <= '3']
 y5 = y[y <= '3']
+
+mlp = MLPClassifier(
+    hidden_layer_sizes=(6,),
+    max_iter=200, alpha=1e-4,
+    solver='sgd', random_state=2)
+
+mlp.fit(X5, y5)
